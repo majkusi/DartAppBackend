93439defae104ed618104629287737461c36a50a
majkusi <igor34p@gmail.com>
2025-12-17 23:13:06 +0100

    Adding Cricket to Match and TeamPlayer entites

--- src/Domain/Entities/GameEntites/TeamPlayer.cs
+++ src/Domain/Entities/GameEntites/TeamPlayer.cs
@@ -11,12 +11,18 @@
         public int IndividualScore { get; set; }
         public bool Winner { get; set; }
         public int Order { get; set; }
-        public float? Average { get; set; }
-        public int CricketMarksCounter { get; set; }
+        public float? X01Average { get; set; }
+        public int RoundCounter { get; set; }
+        public bool isCricket { get; set; } = false;
         public ICollection<int> PointsPerRound { get; set; } = new List<int>();
-        public ICollection<List<int>> CricketPointsPerRound { get; set; } = new List<List<int>>();
-        public void ScorePointsX01(int points)
+        public ICollection<List<int>> CrickerPointsPerRound { get; set; } = new List<List<int>>();
+        public void ScorePoints(int points)
         {
+            if (isCricket == true)
+            {
+
+            }
+
             if (points < 0)
                 throw new ArgumentException("Points cannot be negative.");
 
@@ -29,19 +35,8 @@
             {
                 PointsPerRound.Add(points);
                 IndividualScore -= points;
-                Average = PointsPerRound.Sum() / PointsPerRound.Count();
+                X01Average = PointsPerRound.Sum() / PointsPerRound.Count();
             }
-        }
-        public void ScorePointsCricket(List<int> points)
-        {
-            if (points.Count == 0)
-                throw new ArgumentException("Points cannot be empty");
-            if (points.Count > 9)
-                throw new ArgumentException("Maximum marks per round is 9!");
-
-            CricketPointsPerRound.Add(points);
-            CricketMarksCounter += points.Count();
-            Average = CricketMarksCounter / CricketPointsPerRound.Count();
         }
 
     }
